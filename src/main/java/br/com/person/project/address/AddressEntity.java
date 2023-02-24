474d150fd986b4897155013fb6a2a2224ca82d17
lucasalvess96 <lucasalves.57@hotmail.com>
2023-02-23 21:15:05 -0300

    fix: tratando erro de ids nao encontrado

---
+++ src/main/java/br/com/person/project/address/AddressEntity.java
@@ -0,0 +1,69 @@
+package br.com.person.project.address;
+
+import br.com.person.project.person.PersonEntity;
+import jakarta.persistence.*;
+
+@Entity
+public class AddressEntity {
+    @Id
+    @Column(name = "ADDRESS_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private long number;
+    private String city;
+
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "address")
+    private PersonEntity personEntity;
+
+    public AddressEntity() {
+    }
+
+    public AddressEntity(Long id, String street, long number, String city, PersonEntity personEntity) {
+        this.id = id;
+        this.street = street;
+        this.number = number;
+        this.city = city;
+        this.personEntity = personEntity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public PersonEntity getPersonEntity() {
+        return personEntity;
+    }
+
+    public void setPersonEntity(PersonEntity personEntity) {
+        this.personEntity = personEntity;
+    }
+}
